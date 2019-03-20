f248443bf693be18a7b8276d438d8b0e17983d9b
Krzysztof <kzajac1997@gmail.com>
2019-03-20 18:23:27 +0100

    Update

--- Lab01/MainWindow.xaml.cs
+++ Lab01/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Text.RegularExpressions;
 using System.Linq;
 using System.IO;
 using System.Timers;
+using System.Windows.Interop;
+using System.Drawing;
 
 namespace Lab01
 {
@@ -25,14 +27,26 @@ namespace Lab01
         /// </summary>
         string imagePath;
 
+        /// <summary>
+        /// observable collection displayed in MainWindow
+        /// </summary>
+        public ObservableCollection<Person> Items => people;
         static ObservableCollection<Person> people = new ObservableCollection<Person>
         {
             new Person { Name = "P1", Age = 1 },
             new Person { Name = "P2", Age = 2 }
         };
 
-        public ObservableCollection<Person> Items => people;
+        /// <summary>
+        /// static HttpClient field used not to create
+        /// new instance with every iteration
+        /// </summary>
+        public static HttpClient Client => client;
+        private readonly static HttpClient client = new HttpClient();
 
+        /// <summary>
+        /// Adds new person from web every 5 seconds
+        /// </summary>
         public MainWindow()
         {
             InitializeComponent();
@@ -41,26 +55,27 @@ namespace Lab01
             RunPeriodically(OnTick, TimeSpan.FromSeconds(5), TimeSpan.FromSeconds(5)).ContinueWith(task => { }, TaskScheduler.FromCurrentSynchronizationContext());
         }
 
-        
+        /// <summary>
+        /// Adds New person to observable collection using HttpClient
+        /// Uses Regex to get Webpage title and first encountered int
+        /// </summary>
         private async void AddNewPersonFromWeb()
         {
             try
-            {
-                using (HttpClient client = new HttpClient())
+            {        
+                string result = await Client.GetStringAsync("https://en.wikipedia.org/wiki/Main_Page");
+
+                string name = Regex.Match(result, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
+                string ageString = Regex.Match(result, "[0-9]+").Value;
+                
+                if (int.TryParse(ageString, out int age))
                 {
-                    string result = await client.GetStringAsync("https://en.wikipedia.org/wiki/Main_Page");
-                    string name = Regex.Match(result, @"\<title\b[^>]*\>\s*(?<Title>[\s\S]*?)\</title\>", RegexOptions.IgnoreCase).Groups["Title"].Value;
-                    string ageString = Regex.Match(result, "[0-9]+").Value;
+                    people.Add(new Person { Age = age, Name = name });
+                }
 
-                    if (int.TryParse(ageString, out int age))
-                    {
-                        people.Add(new Person { Age = age, Name = name });
-                    }
-
-                    else
-                    {
-                        MessageBox.Show("Age must be number");
-                    }
+                else
+                {
+                    MessageBox.Show("Age must be number");
                 }
             }
 
@@ -78,6 +93,12 @@ namespace Lab01
             }
         }
 
+        /// <summary>
+        /// Event for AddNewPersonButton
+        /// Adds new person with user's input data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddNewPersonButton_Click(object sender, RoutedEventArgs e)
         {
             if (int.TryParse(ageTextBox.Text, out int age))
@@ -91,6 +112,11 @@ namespace Lab01
             }
         }
 
+        /// <summary>
+        /// Adds photo to person instance
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddNewPhoto_Click(object sender, RoutedEventArgs e)
         {
             OpenFileDialog fileDialog = new OpenFileDialog { Title = "Select a photo" };
@@ -102,8 +128,29 @@ namespace Lab01
             }
         }
 
+        /// <summary>
+        /// Event for AddNewPersonFromWeb
+        /// Adds new person from web manually
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void AddNewPersonFromWeb_Click(object sender, RoutedEventArgs e) => AddNewPersonFromWeb();
 
+        /// <summary>
+        /// Task running periodically calling OnTick event function 
+        /// </summary>
+        /// <param name="OnTick">
+        /// Delegate to event happening OnTicks
+        /// </param>
+        /// <param name="dueTime">
+        /// TimeSpan parameter
+        /// </param>
+        /// <param name="interval">
+        /// Time span paramter setting length of inverval between OnTick calls
+        /// </param>
+        /// <returns>
+        /// Returns Task so it can run asynchronously in MainWindow()
+        /// </returns>
         private async Task RunPeriodically(Action OnTick, TimeSpan dueTime, TimeSpan interval)
         {
             if(dueTime > TimeSpan.Zero)
@@ -122,6 +169,9 @@ namespace Lab01
             }
         }
 
+        /// <summary>
+        /// Adds new Person from web when called 
+        /// </summary>
         private void OnTick() => AddNewPersonFromWeb();
     }
 }
